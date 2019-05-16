83adc8c52408ae254932d9094c3737c5db120075
Nargarcyz <narcyz2048@gmail.com>
2019-05-16 17:40:41 +0200

    kdtree search done

--- bot1.py
+++ bot1.py
@@ -14,6 +14,7 @@ from collections import namedtuple
 from operator import itemgetter
 from pprint import pformat
 from haversine import haversine, Unit
+import math
 
 
 #from fuzzywuzzy import fuzz
@@ -37,9 +38,12 @@ class Tree(object):
             child.print()
 
 
-class Node(namedtuple('Node', 'city population location left_child right_child')):
+
+class Node(namedtuple('Node', 'city population location latlon left_child right_child')):
     def __repr__(self):
         return pformat(tuple(self))
+    def print(self):
+        print(self.city + " " + self.population)
 
 
 
@@ -49,26 +53,79 @@ def kdtree(point_list, depth=0):
         return None
 
     #k = len(point_list[0])  # assumes all points have the same dimension
-    k = 2
+    k = 3
     # Select axis based on depth so that axis cycles through all valid values
     axis = depth % k
 
     # Sort point list by axis and choose median as pivot element
     #point_list.sort(key=itemgetter(axis))
+
+    #x = cos(lat)*cos(lon)
+    #y = cos(lat)*sin(lon)
+    #z = sin(lat)
     if(axis==0):
-        point_list.sort(key=lambda x: float(earthRadius)*float(x['Longitude']))
+        point_list.sort(key=lambda x: math.cos(float(x['Latitude'])) * math.cos(float(x['Longitude'])))
+    elif(axis==1):
+        point_list.sort(key=lambda x: math.cos(float(x['Latitude'])) * math.cos(float(x['Longitude'])))
     else:
-        point_list.sort(key=lambda x: float(earthRadius)*float(x['Latitude']))
+        point_list.sort(key=lambda x: math.sin(float(x['Latitude'])))
     median = len(point_list) // 2
 
+    x = math.cos(float(point_list[median]['Latitude'])) * math.cos(float(point_list[median]['Longitude']))
+    y = math.cos(float(point_list[median]['Latitude'])) * math.sin(float(point_list[median]['Longitude']))
+    z = math.sin(float(point_list[median]['Latitude']))
     # Create node and construct subtrees
     return Node(
-        location=(point_list[median]['Latitude'],point_list[median]['Longitude']),
+        location=(x,y,z),
+        latlon=(float(point_list[median]['Latitude']),float(point_list[median]['Longitude'])),
         city=point_list[median]['City'],
         population=point_list[median]['Population'],
         left_child=kdtree(point_list[:median], depth + 1),
         right_child=kdtree(point_list[median + 1:], depth + 1)
     )
+def dist(pos1,pos2):
+    return math.sqrt(math.pow(pos2[0]-pos1[0],2)+math.pow(pos2[1]-pos1[1],2)+math.pow(pos2[2]-pos1[2],2))
+
+def closeNodes(root, node, latlon, distance, depth=0):
+    outList = []
+    k = 3
+    axis = depth % k
+    if(root is not None):
+        
+        if(root.location[axis]<node[axis]):
+            outList.extend(closeNodes(root.left_child,node,latlon,distance))
+            if(root.location[axis]-node[axis] < distance):
+                outList.extend(closeNodes(root.right_child,node,latlon,distance))
+        elif(root.location[axis]>node[axis]):
+            outList.extend(closeNodes(root.right_child,node,latlon,distance))
+            if(root.location[axis]-node[axis] < distance):
+                outList.extend(closeNodes(root.left_child,node,latlon,distance))
+        else: 
+            pass
+
+        #print(root.latlon)
+        #print(latlon)
+        if(haversine(root.latlon,latlon)<=distance):
+            outList.append(root)
+            
+
+
+
+
+
+        '''if (dist(root.location,node) <= distance):
+            outList.append(root)
+            if(root.left_child is not None and dist(root.left_child.location, node) <= distance):
+                #outList = outList ++ root.left_child ++ closeNodes(root.left_child, node, distance)
+                outList.append(root.left_child)
+                outList.extend(closeNodes(root.left_child,node,distance))
+            if(root.right_child is not None and dist(root.right_child.location, node) <= distance):
+                #outList = outList ++ root.right_child ++ closeNodes(root.right_child, node, distance)
+                outList.append(root.right_child)
+                outList.extend(closeNodes(root.right_child,node,distance))'''
+    return outList
+
+
 
 def treeTest():
     '''t = Tree('*', [Tree('1'),
@@ -82,13 +139,20 @@ def treeTest():
     #print(tree)
     with open('worldcitiespop.json', 'r', encoding="utf8") as json_file:
         jfile = json.load(json_file)
-        print(sorted(jfile,key=lambda x: float(x['Latitude'])))
+        #print(sorted(jfile,key=lambda x: float(x['Latitude'])))
 
-
+        pos = (math.cos(float(jfile[0]['Latitude'])) * math.cos(float(jfile[0]['Longitude'])),math.cos(float(jfile[0]['Latitude'])) * math.sin(float(jfile[0]['Longitude'])) , math.sin(float(jfile[0]['Latitude'])) )
+        latLon = (float(jfile[0]['Latitude']),float(jfile[0]['Longitude']))
+        print(pos[0],pos[1])
         tree = kdtree(jfile)
+        nodesCloseToAndorra = closeNodes(tree,pos,latLon,10)
+        for node in nodesCloseToAndorra:
+            node.print()
 
 
-        m = StaticMap(2000, 2000, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+
+
+        '''m = StaticMap(2000, 2000, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
         for city in jfile:
             if(float(city['Population'])>100000):
                 print(city['City'])
@@ -104,7 +168,7 @@ def treeTest():
 
 
         image = m.render(zoom=5)
-        image.save('map.png')
+        image.save('map.png')'''
 
 
 
